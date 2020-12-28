a91b4f9e127f1c0915f8fe094102628a707290bb
GlebUliv <55991627+GlebUliv@users.noreply.github.com>
2020-12-28 18:58:03 +0200

    Changes in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+
+    // JS for burger
     $(".header__burger").click(function(event){
         $(".header__burger,.wrapper").toggleClass('active')
     });
+
+    // Smooth movement to a
+    $(".menu__box").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+    
 });
